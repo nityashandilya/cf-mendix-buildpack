cf2c1397d8cc09d118d1ed0a6086f398be658115
Denis Shuvalov <denis.shuvalov@mendix.com>
2022-03-10 18:11:07 +0100

    python coinvertor

--- buildpack/telemetry/appdynamics_telegraf_output.py
+++ buildpack/telemetry/appdynamics_telegraf_output.py
@@ -37,7 +37,12 @@ def convert_payload():
 
 if __name__ == "__main__":
 
-    convert_payload()
+    try:
+        convert_payload()
+    except Exception as e:
+        with open("/tmp/err.txt", "w") as f:
+
+            f.write(e)
 
 #{"metrics":[{"fields":{"value": 0.016489,},"name":"process_cpu_usage","tags":{"app_name":"d5aa4abb-c2dc-4c9d-8a4d-f6c8e1e5a53d","host":"nonprod4denis-test.dev.mendixcloud.com-0","instance_index":"0","metric_type":"gauge","micrometer_metrics":true},"timestamp":1646922810}]}
 
