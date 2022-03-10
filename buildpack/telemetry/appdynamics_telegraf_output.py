8b4fb4b12ce7ba565f18cada9e55f6df401c4fc7
Denis Shuvalov <denis.shuvalov@mendix.com>
2022-03-10 17:57:55 +0100

    python coinvertor

--- buildpack/telemetry/appdynamics_telegraf_output.py
+++ buildpack/telemetry/appdynamics_telegraf_output.py
@@ -10,6 +10,7 @@ AGGR_TYPE = "AVERAGE"
 def convert_payload():
 
     metrics_str = input()
+    print(metrics_str)
     metrics_dict = json.loads(metrics_str)
     metrics_list = metrics_dict.get("metrics")
 
@@ -22,7 +23,7 @@ def convert_payload():
     for metric in metrics_list:
 
         appd_metric = {
-            "metricName": "|".join((METRIC_TREE_PATH, matric["name"])),
+            "metricName": "|".join((METRIC_TREE_PATH, metric["name"])),
             "aggregatorType": AGGR_TYPE,
             "value": int(metric["fields"]["value"])
         }
@@ -38,21 +39,6 @@ if __name__ == "__main__":
 
     convert_payload()
 
-a = {"metrics":
-    [
-        {
-        "fields":{
-            "value": 0.016489,
-        },
-        "name":"process_cpu_usage",
-        "tags":{
-            "app_name":"d5aa4abb-c2dc-4c9d-8a4d-f6c8e1e5a53d",
-            "host":"nonprod4denis-test.dev.mendixcloud.com-0",
-            "instance_index":"0",
-            "metric_type":"gauge",
-            "micrometer_metrics":"true"
-        },
-        "timestamp":1646922810
-        }
-    ]
-}
+#{"metrics":[{"fields":{"value": 0.016489,},"name":"process_cpu_usage","tags":{"app_name":"d5aa4abb-c2dc-4c9d-8a4d-f6c8e1e5a53d","host":"nonprod4denis-test.dev.mendixcloud.com-0","instance_index":"0","metric_type":"gauge","micrometer_metrics":true},"timestamp":1646922810}]}
+
+# {"metrics":[{"fields":{"value": 0.016489},"name":"process_cpu_usage"}]}
