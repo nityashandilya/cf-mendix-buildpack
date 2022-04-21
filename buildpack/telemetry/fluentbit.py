e670f369075df529155b96dc09fed09aacc1965b
Denis Shuvalov <denis.shuvalov@mendix.com>
2022-04-21 14:38:56 +0200

    Only bin file

--- buildpack/telemetry/fluentbit.py
+++ buildpack/telemetry/fluentbit.py
@@ -5,10 +5,7 @@ from buildpack import util
 
 
 NAMESPACE = "fluentbit"
-FLUENTBIT_VERSION = "1.9.1"
-FLUENTBIT_ARCHIVE_NAME = "fluent-bit-{}-compiled.tar.gz".format(
-    FLUENTBIT_VERSION
-)
+FLUENTBIT_BIN_NAME = "fluent-bit"
 
 
 def stage(buildpack_dir, destination_path, cache_path):
@@ -16,7 +13,7 @@ def stage(buildpack_dir, destination_path, cache_path):
     logging.info("Staging Fluent Bit log processor...")
 
     fluentbit_cdn_path = os.path.join(
-        "/mx-buildpack", NAMESPACE, FLUENTBIT_ARCHIVE_NAME
+        "/mx-buildpack", NAMESPACE, FLUENTBIT_BIN_NAME
     )
 
     util.resolve_dependency(
@@ -25,4 +22,5 @@ def stage(buildpack_dir, destination_path, cache_path):
         os.path.join(destination_path, NAMESPACE),
         buildpack_dir=buildpack_dir,
         cache_dir=cache_path,
+        unpack=False,
     )
